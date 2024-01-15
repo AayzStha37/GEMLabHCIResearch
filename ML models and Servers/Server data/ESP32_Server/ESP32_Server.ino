f00b8774b177e32b3e6cc10dd9de9ca87677333c
Aayush Shrestha <saayush97@gmail.com>
2024-01-15 19:47:16 -0400

    1. Generic oscillator program for the Daisy added | 2. ESP32 server program added | 3. Falsk server update | 4. Connection between ESP32 and Daisy tested

---
+++ ML models and Servers/Server data/ESP32_Server/ESP32_Server.ino
@@ -0,0 +1,108 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <SPI.h>
+
+//SPI pins
+// MOSI - 35
+// MISO - 37
+// SCK - 36
+// CS - 14
+
+#define CS_PIN  14
+
+const char* ssid = "Humans Wifi"; 
+const char* password = "HBo@2489";
+const char* PARAM_MESSAGE = "message";
+
+AsyncWebServer server(80);
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(CS_PIN, OUTPUT);
+    // Initialize SPI
+    SPI.begin();
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("SUCCESS: Connected to WiFi.");
+    Serial.println("Listening to client requests...");
+
+    handleEndPoints();
+    server.begin();
+}
+
+void handleEndPoints(){
+
+  // Recieve message from the flask client
+  server.on("/receive_message", HTTP_POST, [](AsyncWebServerRequest *request) {
+    // Get the raw content of the request
+    String message;
+    //printRequestParameters(request);
+    // Check if the PARAM_MESSAGE parameter exists in the request
+    if (request->hasParam(PARAM_MESSAGE, true)) {
+        // Get the value of the PARAM_MESSAGE parameter
+        message = request->getParam(PARAM_MESSAGE, true)->value();
+        Serial.println("Received message from Flask server: " + message);
+        request->send(200, "text/plain", "Message received by ESP32");
+        sendDataToDaisy();
+
+    } else {
+        Serial.println("Message not found in the request");
+        request->send(400, "text/plain", "PARAM_MESSAGE not found in the request");
+    }
+});
+
+}
+
+
+void sendDataToDaisy(){
+  // digitalWrite(CS_PIN, LOW);
+  // // Send a message byte by byte
+  // const char* message = "Hello!";
+  // for (int i = 0; i < strlen(message); i++) {
+  //   SPI.transfer(message[i]);
+  // }
+  digitalWrite(CS_PIN, HIGH);
+
+  delay(1000);
+
+  digitalWrite(CS_PIN, LOW);
+}
+
+// void printRequestParameters(AsyncWebServerRequest *request) {
+//   // Print basic information
+//   Serial.print("Request Method: ");
+//   Serial.println(request->methodToString());
+  
+//   Serial.print("Request URI: ");
+//   Serial.println(request->url());
+
+//   // Print headers
+//   Serial.println("Headers:");
+//   int headerCount = request->headers();
+//   for (int i = 0; i < headerCount; i++) {
+//     AsyncWebHeader* header = request->getHeader(i);
+//     Serial.print(header->name());
+//     Serial.print(": ");
+//     Serial.println(header->value());
+//   }
+//   Serial.println("POST Parameters:");
+//     int postParamsCount = request->params();
+//     for (int i = 0; i < postParamsCount; i++) {
+//       AsyncWebParameter* param = request->getParam(i);
+//       Serial.print(param->name());
+//       Serial.print(": ");
+//       Serial.println(param->value());
+//     }
+// }
+
+
+
+void loop() {
+    // Your ESP32 code here
+}
---
+++ ML models and Servers/Server data/Generic_oscillator_program/Generic_oscillator_program.ino
@@ -0,0 +1,121 @@
+
+#include "DaisyDuino.h"
+#include <SPI.h>
+#include <SPIExtension.h>
+
+DaisyHardware hw;
+
+size_t num_channels;
+
+static Oscillator osc_sine;
+static Oscillator osc_tri;
+static Oscillator osc_toPlay;
+
+const String SINE = "sine";
+const String TRI = "tri";
+
+//SPI pins
+// MOSI - 10
+// MISO - 9
+// SCK - 8
+// CS - 7
+
+const uint8_t PIN_CS = 7;
+const uint8_t NUM_DEVICE = 1;
+SPIExtension<PIN_CS, NUM_DEVICE> spi;
+
+void MyCallback(float **in, float **out, size_t size) {
+  Oscillator osc = osc_toPlay;
+  for (size_t i = 0; i < size; i++) {
+    float sig = osc.Process();
+
+    for (size_t chn = 0; chn < num_channels; chn++) {
+      out[chn][i] = sig;
+    }
+  }
+}
+
+void setup() {  
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_CS, INPUT);
+
+  SPI.begin();
+  spi.attatch(SPI);
+  float sample_rate;
+  // Initialize for Daisy pod at 96kHz
+  hw = DAISY.init(DAISY_SEED, AUDIO_SR_96K);
+  num_channels = hw.num_channels;
+  sample_rate = DAISY.get_samplerate();
+
+  initOscillators(sample_rate);
+}
+
+void initOscillators(float sample_rate){
+  
+  osc_sine.Init(sample_rate);
+  osc_sine.SetFreq(440);
+  osc_sine.SetAmp(0.5);
+  osc_sine.SetWaveform(Oscillator::WAVE_SIN);
+  
+  osc_tri.Init(sample_rate);
+  osc_tri.SetFreq(200);
+  osc_tri.SetAmp(2);
+  osc_tri.SetWaveform(Oscillator::WAVE_TRI);
+}
+
+void loop() {
+  // if (Serial.available()) {
+  //   String serialInput = Serial.readStringUntil('\n');
+  //   serialInput.trim();
+
+  //   if (serialInput.equalsIgnoreCase(SINE)) {
+  //     DAISY.end();
+  //     Serial.println("Received command: Sine Wave");
+  //     osc_toPlay = osc_sine;
+  //     DAISY.begin(MyCallback);
+      
+  //   } else if (serialInput.equalsIgnoreCase(TRI)) {
+  //     DAISY.end();
+  //     Serial.println("Received command: Triangle Wave");
+  //     osc_toPlay = osc_tri;      
+  //     DAISY.begin(MyCallback);
+
+  //   } else {
+  //     Serial.println("Invalid command");
+  //   }
+  // }
+
+  //read data over SPI connection
+  // for (uint8_t i = 0; i < spi.size(); ++i)
+  // {
+  //   Serial.println(spi.data(i), HEX); 
+  // }
+
+  static int previousState = LOW; // Assume the initial state is LOW
+  int currentState = digitalRead(PIN_CS);
+
+  if (currentState != previousState) {
+    // CS pin state has changed
+    if (currentState == HIGH) {
+      osc_toPlay = osc_sine;      
+      DAISY.begin(MyCallback);
+      Serial.println("CS pin is HIGH");
+      blinkLED();
+    } else {
+      DAISY.end();
+      Serial.println("CS pin is LOW");
+    }
+
+    // Update the previous state
+    previousState = currentState;
+  }
+}
+
+
+void blinkLED(){
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);   
+}
